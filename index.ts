3c431e5716d18a4a0de724cbfc41e6df40fce055
Jean-Luc AMITOUSA <jean-luc.amitousa@almaviacx.com>
2023-10-23 23:57:36 +0200

    testing es6 compilation

--- index.ts
+++ index.ts
@@ -1,1 +1,63 @@
-console.log("Hello World");
+import type { Generate } from '@llama-node/llama-cpp';
+import { LLM } from 'llama-node';
+import { LLamaCpp, type LoadConfig } from "llama-node/dist/llm/llama-cpp.js";
+import path from 'path';
+
+const model = path.resolve(process.cwd(), "../ggml-vic7b-q5_1.bin");
+
+const llama = new LLM(LLamaCpp);
+
+const config: LoadConfig = {
+    modelPath: model,
+    enableLogging: true,
+    nCtx: 1024,
+    seed: 0,
+    f16Kv: false,
+    logitsAll: false,
+    vocabOnly: false,
+    useMlock: false,
+    embedding: false,
+    useMmap: true,
+    nGpuLayers: 0
+};
+
+const template = `How are you?`;
+
+const prompt = `A chat between a user and an assistant.
+USER: ${template}
+ASSISTANT:`;
+
+const params: Generate = {
+    nThreads: 4,
+    nTokPredict: 2048,
+    topK: 40,
+    topP: 0.1,
+    temp: 0.2,
+    repeatPenalty: 1,
+    prompt,
+};
+
+const run = async () => {
+
+    console.log("Hello World 1 ");
+
+    await llama.load(config);
+
+    console.log("Hello World 2 ");
+
+
+    await llama.createCompletion(params, (response) => {
+
+        console.log("Hello World 3 ");
+
+        process.stdout.write(response.token);
+
+        console.log("Hello World 4 ");
+
+    });
+
+    console.log("Hello World 5 ");
+
+};
+
+run();
